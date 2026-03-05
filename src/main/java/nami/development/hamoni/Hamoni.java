f7c0546ad6c6b856d17656a023ebd157fb5b9e6b
Kiriyaga W <kiriyaga7615@github.com>
2026-03-05 19:19:39 +0100

    init

---
+++ src/main/java/nami/development/hamoni/Hamoni.java
@@ -0,0 +1,10 @@
+package nami.development.hamoni;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Hamoni implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
